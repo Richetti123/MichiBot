dacf373f94a76a9acfd9acf8158e050fa8de15c3
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-04-16 00:14:46 +0200

    Add files via upload

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,8 +1,8 @@
 import fg from 'api-dylux';
 import fetch from 'node-fetch';
-import {savefrom, facebookdl, facebookdlv2} from '@bochilteam/scraper';
-import fbDownloader from 'fb-downloader-scrapper';
-import {facebook} from '@xct007/frieren-scraper';
+//import {savefrom, facebookdl, facebookdlv2} from '@bochilteam/scraper';
+//import fbDownloader from 'fb-downloader-scrapper';
+//import {facebook} from '@xct007/frieren-scraper';
 import axios from 'axios';
 const handler = async (m, {conn, args, command, usedPrefix}) => {
 let user = global.db.data.users[m.sender]
@@ -11,7 +11,29 @@ if (!args[0].match(/www.facebook.com|fb.watch/g)) return conn.reply(m.chat, `${l
 let contenido = `✅ 𝙑𝙄𝘿𝙀𝙊 𝘿𝙀 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\n${wm}`
 await m.react('⏱️')
 try {
-const apiUrl = `https://api.dorratz.com/fbvideo?url=${encodeURIComponent(args[0])}`;
+const api = await fetch(`${global.APIs.neoxr.url}/fb?url=${args}&apikey=${global.APIs.neoxr.key}`);
+const response = await api.json();
+if (response.status && Array.isArray(response.data)) {
+const videoHD = response.data.find(video => video.quality === "HD")?.url;
+const videoSD = response.data.find(video => video.quality === "SD")?.url;
+const videoUrl = videoHD || videoSD;
+await conn.sendFile(m.chat, videoUrl, 'video.mp4', contenido, m, null, fake);
+m.react('✅');
+}} catch {   
+try {
+const api = await fetch(`https://api.agatz.xyz/api/facebook?url=${args}`);
+const data = await api.json();
+const videoUrl = data.data.hd || data.data.sd; 
+const imageUrl = data.data.thumbnail; 
+if (videoUrl && videoUrl.endsWith('.mp4')) {
+await conn.sendFile(m.chat, videoUrl, 'video.mp4', `${gt}`, m, null, fake);
+m.react('✅');
+} else if (imageUrl && (imageUrl.endsWith('.jpg') || imageUrl.endsWith('.png'))) {
+await conn.sendFile(m.chat, imageUrl, 'thumbnail.jpg', contenido, m, null, fake);
+m.react('✅');
+}} catch {   
+try {
+const apiUrl = `https://api.dorratz.com/fbvideo?url=${args}`;
 const response = await fetch(apiUrl);
 const data = await response.json();
 if (data.result) {
@@ -20,48 +42,24 @@ const sdUrl = data.result.sd;
 const audioUrl = data.result.audio;        
 const downloadUrl = hdUrl || sdUrl; 
 await conn.sendFile(m.chat, downloadUrl, `error.mp4`, contenido, m)
-}} catch (err1) {
+}} catch {   
 try {
-const apiUrl = `${apis}/download/facebook?url=${encodeURIComponent(args[0])}`;
+const apiUrl = `${apis}/download/facebook?url=${args}`;
 const apiResponse = await fetch(apiUrl);
 const delius = await apiResponse.json();
 if (!delius || !delius.urls || delius.urls.length === 0) return m.react("❌")
 const downloadUrl = delius.urls[0].hd || delius.urls[0].sd;
 if (!downloadUrl) return m.react("❌");
 await conn.sendFile(m.chat, downloadUrl, `error.mp4`, contenido, m)
-} catch (err2) {
-try {
-const d2ata = await facebook.v1(args[0]);
-let r2es = '';
-if (d2ata.urls && d2ata.urls.length > 0) {
-r2es = `${d2ata.urls[0]?.hd || d2ata.urls[1]?.sd || ''}` }
-conn.sendFile(m.chat, r2es, `error.mp4`, contenido, m)
-m.react(`✅`) 
-} catch (err3) {
-try {
-const req = await igeh(args[0]);
-conn.sendMessage(m.chat, {video: {url: req.url_list}}, m);
-} catch (err5) {
+} catch {   
 try {
 const ress = await fg.fbdl(args[0]);
 const urll = await ress.data[0].url;
 await conn.sendFile(m.chat, urll, `error.mp4`, contenido, m)
-m.react(`✅`) 
-} catch (err6) {
-try {
-const res = await fbDownloader(args[0]);
-for (const result of res.download) {
-const ur = result.url;
-await conn.sendFile(m.chat, ur, `error.mp4`, contenido, m) 
-m.react(`✅`)               
-}} catch (err7) {
-try {
-const {result} = await facebookdl(args[0]).catch(async (_) => await facebookdlv2(args[0])).catch(async (_) => await savefrom(args[0]));
-for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `error.mp4`, contenido, m) 
-m.react(`✅`)                 
+m.react(`✅`)                
 } catch (e) {   
 console.log(e) 
-}}}}}}}}
+}}}}}}
 handler.command = /^(facebook|fb|facebookdl|fbdl|facebook1|fb1|facebookdl1|fbdl1|facebook2|fb2|facebookdl2|fbdl2)$/i
 handler.limit = 3
 export default handler
