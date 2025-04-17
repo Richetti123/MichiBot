8c09d5a507764c6f8701bf20616df1dca1d39f25
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-04-17 03:19:14 +0200

    Update grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -1,25 +1,12 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let mentionedJid = [who]
-let username = conn.getName(who)
-let group = m.chat
-
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
-let fsizedoc = '1'.repeat(10)
-let adReply = { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: { forwardingScore: fsizedoc, externalAdReply: { showAdAttribution: true, title: wm, body: '👋 ' + username, mediaUrl: ig, description: 'Hola', previewType: 'PHOTO', thumbnail: await(await fetch(gataMenu)).buffer(), sourceUrl: redesMenu }}}
-
-try{
-///await conn.sendFile(m.chat, pp, 'error.jpg', '*https://chat.whatsapp.com/' + await conn.groupInviteCode(group) + '*', m)
-await conn.sendButton(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), wm, pp, [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], null, null, fkontak, adReply)
-} catch (e) {
-await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-}} 
-handler.help = ['linkgroup']
-handler.tags = ['group']
-handler.command = /^enlace|link(gro?up)?$/i
-handler.group = true
-handler.botAdmin = true
-export default handler
+import fs from 'fs';
+const handler = async (m, {conn, args}) => {
+const group = m.chat;
+m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group)) 
+};
+handler.help = ['linkgroup'];
+handler.tags = ['group'];
+handler.command = /^link(gro?up)?$/i;
+handler.group = true;
+handler.botAdmin = true;
+handler.register = true 
+export default handler;
