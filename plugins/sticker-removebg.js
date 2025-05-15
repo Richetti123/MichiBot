c9d615f5af9df3b850cbd0a461404d60714b8a92
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-05-16 01:53:25 +0200

    Update sticker-removebg.js

--- plugins/sticker-removebg.js
+++ plugins/sticker-removebg.js
@@ -22,7 +22,7 @@ json = await (await fetch(`https://btch.us.kg/removebg?url=${text.trim()}`)).jso
 //await mensajesEditados(conn, m)
 //await conn.sendMessage(m.chat, { text: waitttttt, edit: key })
 await conn.sendMessage(m.chat, { image: { url: json.result.urls }, caption: null }, { quoted: m })
-await conn.sendFile(m.chat, stiker ? stiker : await sticker(false, json.result.urls, global.packname, global.author), 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 `, mediaType: 2, sourceUrl: accountsgb, thumbnail: imagen1}}}, { quoted: m })
+await conn.sendFile(m.chat, stiker ? stiker : await sticker(false, json.result.urls, global.packname, global.author), 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: wm, mediaType: 2, sourceUrl: accountsgb, thumbnail: imagen1}}}, { quoted: m })
 }
 handler.command = /^(s?removebg)$/i
 export default handler
