824352e9e72d88847c0b98ddc129a39db344f95f
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-05-19 03:34:49 +0200

    Update grupo-setpagos.js

--- plugins/grupo-setpagos.js
+++ plugins/grupo-setpagos.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
 if (text) {
-global.db.data.chats[m.chat].sPagos = text
+global.db.data.chats[m.chat].pagos = text
 conn.reply(m.chat, `*TU METODO DE PAGO HA SIDO CONFIGURADO CORRECTAMENTE*`, fkontak, m)
 } else throw `*PARA CONFIGURAR TU METODO DE PAGO EN ESTE GRUPO USA .setpagos*`
 }
