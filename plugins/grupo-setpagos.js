c543ec17f35e4f58bbc8c5be58c0f18f9cae02b3
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-05-19 02:32:25 +0200

    Add files via upload

---
+++ plugins/grupo-setpagos.js
@@ -0,0 +1,11 @@
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+if (text) {
+global.db.data.chats[m.chat].smsSsetpagos = text
+conn.reply(m.chat, lenguajeGB.smsSetP(), fkontak, m)
+} else throw `${lenguajeGB['smsSetP2']()}`
+}
+handler.command = ['setpagos'] 
+handler.botAdmin = true
+handler.admin = true
+handler.group = true
+export default handler
