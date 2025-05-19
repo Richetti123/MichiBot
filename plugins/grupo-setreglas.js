d008480d12b907c37664cb0c7789faeeb27a9390
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-05-19 02:33:31 +0200

    Add files via upload

---
+++ plugins/grupo-setreglas.js
@@ -0,0 +1,11 @@
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+if (text) {
+global.db.data.chats[m.chat].smsSsetpagos = text
+conn.reply(m.chat, lenguajeGB.smsSetR(), fkontak, m)
+} else throw `${lenguajeGB['smsSetR2']()}`
+}
+handler.command = ['setreglas'] 
+handler.botAdmin = true
+handler.admin = true
+handler.group = true
+export default handler
---
+++ plugins/grupo-setstock.js
@@ -0,0 +1,11 @@
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+if (text) {
+global.db.data.chats[m.chat].smsSsetpagos = text
+conn.reply(m.chat, lenguajeGB.smsSetS(), fkontak, m)
+} else throw `${lenguajeGB['smsSetS2']()}`
+}
+handler.command = ['setstock'] 
+handler.botAdmin = true
+handler.admin = true
+handler.group = true
+export default handler
