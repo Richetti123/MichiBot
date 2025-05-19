d82a062bedd509c000c74e150d50f5072e4b46c6
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-05-19 02:54:59 +0200

    Update grupo-setstock.js

--- plugins/grupo-setstock.js
+++ plugins/grupo-setstock.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
 if (text) {
 global.db.data.chats[m.chat].sStock = text
-conn.reply(m.chat, lenguajeGB.smsSetS(), fkontak, m)
-} else throw `${lenguajeGB['smsSetS2']()}`
+conn.reply(m.chat, `*TU METODO DE PAGO HA SIDO CONFIGURADO CORRECTAMENTE*`, fkontak, m)
+} else throw `*PARA CONFIGURAR TU METODO DE PAGO EN ESTE GRUPO USA .setstock*`
 }
 handler.command = ['setstock'] 
 handler.botAdmin = true
