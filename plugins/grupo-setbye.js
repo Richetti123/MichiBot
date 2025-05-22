b1fca1c5b833e39d1ae752678d7ca58a4a25d88f
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-05-22 18:44:24 +0200

    Update grupo-setbye.js

--- plugins/grupo-setbye.js
+++ plugins/grupo-setbye.js
@@ -6,7 +6,6 @@ conn.reply(m.chat, lenguajeGB.smsSetB(), fkontak, m)
 } else throw `${lenguajeGB['smsSetB2']()}`
 }
 handler.command = ['setbye', 'despedida'] 
-handler.botAdmin = true
 handler.admin = true
 handler.group = true
 export default handler
