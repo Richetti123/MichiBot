e14eed28f48e8626bdaf21c8bafe16451e47acd9
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-05-22 18:36:41 +0200

    Update grupo-add.js

--- plugins/grupo-add.js
+++ plugins/grupo-add.js
@@ -17,7 +17,6 @@ handler.tags = ['group']
 handler.command = /^(add|agregar|invitar|invite|añadir|\+)$/i
 handler.group = true
 handler.admin = true
-handler.botAdmin = true
 handler.fail = null
 export default handler
 //import fetch from 'node-fetch'
