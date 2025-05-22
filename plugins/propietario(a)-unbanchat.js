e7377926b4d3fd3217c521dd412ecd434f44cb01
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-05-22 18:54:49 +0200

    Update propietario(a)-unbanchat.js

--- plugins/propietario(a)-unbanchat.js
+++ plugins/propietario(a)-unbanchat.js
@@ -5,6 +5,5 @@ global.db.data.chats[m.chat].isBanned = false
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
 handler.command = /^unbanchat$/i;
-handler.botAdmin = true
 handler.admin = true
 export default handler;
