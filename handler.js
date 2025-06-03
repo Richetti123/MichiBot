1f2d6d6b71e0fd3bf6d0e6829d9e34193bc96cc0
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-06-04 01:41:52 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -1066,8 +1066,8 @@ inicio: "00:00",
 fin: "23:59"
 }
 }
-let settings = global.db.data.settings[this.user.jid]
 if (typeof settings !== 'object') global.db.data.settings[this.user.jid] = {}
+if (typeof settings !== 'object') global.db.data.settings[detectwhatt] = {}
 if (settings) {
 if (!('self' in settings)) settings.self = false
 if (!('autoread' in settings)) settings.autoread = false
@@ -1150,6 +1150,8 @@ let _user = global.db.data && global.db.data.users && global.db.data.users[m.sen
 
 const groupMetadata = (m.isGroup ? ((conn.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {}) || {}
 const participants = (m.isGroup ? groupMetadata.participants : []) || []
+let numBot = conn.user.lid.replace(/:.*/, '') || false
+const detectwhat2 = m.sender.includes('@lid') ? `${numBot}@lid` : conn.user.jid
 const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.sender) : {}) || {} // User Data
 const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == this.user.jid) : {}) || {}
 const isRAdmin = user?.admin == 'superadmin' || false
